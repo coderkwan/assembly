2d230d44da2da342c65bac0ce314066f9f9a367b
Kwanele Gamedze <kwanelegamedze4@gmail.com>
2025-01-20 21:48:39 +0200

    fix build

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type {Metadata} from "next";
 import {Geist, Geist_Mono} from "next/font/google";
-import logo from "../public/logo_eswatini_assembly.png"
 import "./globals.css";
 
 const geistSans = Geist({
